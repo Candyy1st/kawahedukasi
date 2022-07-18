0b179120383fa6f74747183bd7d4ea956f15091c
Candyy1st <49828093+Candyy1st@users.noreply.github.com>
2022-07-18 17:03:52 +0700

    Update soal2.php
    update test_soal2.php

--- soal2.php
+++ soal2.php
@@ -1,43 +1,37 @@
 <?php
-/*
-    Tugas Soal No 2
-        Buatlah program untuk pengecekan tipe data:
+//     Tugas Soal No 2
+//         Buatlah program untuk pengecekan tipe data:
 
-        var data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
+//         var data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
 
-        hasil yang di inginkan: 
-        - index ke 0 adalah integer dengan data 1
-        - index ke 1 adalah string dengan data kawah
-        - dan jika nilai dari index itu kurang dari  0 maka keluarkan output:  Tidak bisa di validasi sistem
+//         hasil yang di inginkan: 
+//         - index ke 0 adalah integer dengan data 1
+//         - index ke 1 adalah string dengan data kawah
+//         - dan jika nilai dari index itu kurang dari  0 maka keluarkan output:  Tidak bisa di validasi sistem
 
-        contoh : 
-        var contoh = [1, "mantap", 0 ];
-        expektasi output: 
-        - index ke 0 adalah integer dengan data 1
-        - index ke 1 adalah string dengan data mantap
-        - index ke 2 Tidak bisa di validasi sistem
+//         contoh : 
+//         var contoh = [1, "mantap", 0 ];
+//         expektasi output: 
+//         - index ke 0 adalah integer dengan data 1
+//         - index ke 1 adalah string dengan data mantap
+//         - index ke 2 Tidak bisa di validasi sistem
 
-        buatlah programnya sampai semua data tervalidasi
-*/
+//         buatlah programnya sampai semua data tervalidasi
 
-    $data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1, 9.2];
+    $data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
     
 
     for($i=0; $i<count($data); $i++){
-            echo "index ke ". $i. " adalah ";
-            
-            if (is_integer($data[$i])==TRUE){
-                echo "integer ";}
+        
+        if (is_int($data[$i]) && $data[$i] > 0){
+            echo "index ke-". $i ." adalah integer dengan data ". $data[$i] ."\n";}
 
-            elseif(is_string($data[$i])==TRUE)
-                {echo "string ";}
+        elseif(is_string($data[$i]) && $data[$i] > 0){
+            echo "index ke-". $i ." adalah string dengan data ". $data[$i] ."\n";}
 
-            else{
-                echo "Tidak bisa divalidasi sistem ";
-            }
-
-            echo "dengan data ".$data[$i];
-            echo "\n";
+        else{
+            echo "index ke-". $i ." Tidak bisa divalidasi sistem ";
+        }
     }
 ?>
 
